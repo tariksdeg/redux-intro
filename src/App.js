b7f557b1ba95f353acb5ffbd53777485f8c1e587
tariksdeg <tariksdegirmencii@gmail.com>
2022-12-26 16:55:26 +0300

    REDUX FİRST PROJECT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Counter from './components/Counter';
+import IncreaseCounter from "./components/IncreaseCounter"
+import DecreaseCounter from "./components/DecreaseCounter"
+import IncreaseByTwoCounter from "./components/IncreaseByTwoCounter"
+export default class App extends Component {
+  render() {
+    return (
+      <div >
+        <Counter/>
+        <IncreaseCounter/>
+        <DecreaseCounter/>
+        <IncreaseByTwoCounter/>
+      </div>
+    );
+  }
 }
+  
+  
 
-export default App;
+
